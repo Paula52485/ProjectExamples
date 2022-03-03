442307a9c9f703470e4f4bd761ea91674988dbb8
Paula52485 <98774759+Paula52485@users.noreply.github.com>
2022-03-03 09:12:10 +0000

    Grafica

--- docs/index.md
+++ docs/index.md
@@ -1,1 +1,3 @@
-This is my page. My name is Paula 
+My name is Paula Rueda
+
+Hello again trying ...!!
--- docs/test.fsx
+++ docs/test.fsx
@@ -1,3 +1,21 @@
 (**This is my page*)
 
-let list = [1;2;3]
+
+#load "YahooFinance.fsx"
+
+open YahooFinance
+
+let twnk = YahooFinance.PriceHistory("TWNK")
+
+twnk.[..3]
+|>List.take 3 
+
+#r "nuget: Plotly.NET,2.0.0-preview.17"
+
+open Plotly.NET
+twnk
+|> List.map(fun day -> day.Date, day.AdjustedClose)
+|>Chart.Line
+|>GenericChart.toChartHTML
+
+(**include-it-raw**)
